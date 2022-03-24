e95625ad0d85bbb8a0961528e526a380d9278a59
Nicolas Ferreira <nicolasferreira100@gmail.com>
2022-03-24 13:55:11 -0300

    Fixes

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,17 +1,15 @@
-const BG_COLOUR = '#231f20'
+const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
 const socket = io('https://powerful-hollows-37907.herokuapp.com/');
 
-
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
 socket.on('gameOver', handleGameOver);
 socket.on('gameCode', handleGameCode);
-socket.on('unknownGame', handleUnknownGame);
+socket.on('unknownCode', handleUnknownCode);
 socket.on('tooManyPlayers', handleTooManyPlayers);
-
 
 const gameScreen = document.getElementById('gameScreen');
 const initialScreen = document.getElementById('initialScreen');
@@ -20,117 +18,117 @@ const joinGameBtn = document.getElementById('joinGameButton');
 const gameCodeInput = document.getElementById('gameCodeInput');
 const gameCodeDisplay = document.getElementById('gameCodeDisplay');
 
+newGameBtn.addEventListener('click', newGame);
+joinGameBtn.addEventListener('click', joinGame);
+
+
+function newGame() {
+  socket.emit('newGame');
+  init();
+}
+
+function joinGame() {
+  const code = gameCodeInput.value;
+  socket.emit('joinGame', code);
+  init();
+}
+
 let canvas, ctx;
 let playerNumber;
 let gameActive = false;
 
-newGameBtn.addEventListener('click', newGame);
-joinGameBtn.addEventListener('click', joinGame);
-
-function newGame() {
-    socket.emit('newGame');
-    init();
-};
-
-function joinGame() {
-    const code = gameCodeInput.value;
-    socket.emit('joinGame', code);
-    init();
-};
-
 function init() {
-    initialScreen.style.display = "none";
-    gameScreen.style.display = "block";
+  initialScreen.style.display = "none";
+  gameScreen.style.display = "block";
 
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
 
-    canvas.width = canvas.height = 600;
+  canvas.width = canvas.height = 600;
 
-    ctx.fillStyle = BG_COLOUR;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = BG_COLOUR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    document.addEventListener('keydown', keydown);
-    gameActive = true;
+  document.addEventListener('keydown', keydown);
+  gameActive = true;
 }
 
 function keydown(e) {
-    socket.emit('keydown', e.keyCode);
+  socket.emit('keydown', e.keyCode);
 }
 
-
 function paintGame(state) {
-    ctx.fillStyle = BG_COLOUR;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = BG_COLOUR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    const food = state.food;
-    const gridsize = state.gridsize;
-    const size = canvas.width / gridsize;
+  const food = state.food;
+  const gridsize = state.gridsize;
+  const size = canvas.width / gridsize;
 
-    ctx.fillStyle = FOOD_COLOUR;
-    ctx.fillRect(food.x * size, food.y * size, size, size);
+  ctx.fillStyle = FOOD_COLOUR;
+  ctx.fillRect(food.x * size, food.y * size, size, size);
 
-    paintPlayer(state.player[0], size, SNAKE_COLOUR);
-    paintPlayer(state.player[1], size, 'red');
-
-
+  paintPlayer(state.players[0], size, SNAKE_COLOUR);
+  paintPlayer(state.players[1], size, 'red');
 }
 
 function paintPlayer(playerState, size, colour) {
-    const snake = playerState.snake;
+  const snake = playerState.snake;
 
-
-    ctx.fillStyle = colour;
-    for (let cell of snake) {
-        ctx.fillRect(cell.x * size, cell.y * size, size, size)
-    }
+  ctx.fillStyle = colour;
+  for (let cell of snake) {
+    ctx.fillRect(cell.x * size, cell.y * size, size, size);
+  }
 }
 
-
 function handleInit(number) {
-    playerNumber = number;
-
-};
+  playerNumber = number;
+}
 
 function handleGameState(gameState) {
-    if (!gameActive) {
-        return
-    }
-    gameState = JSON.parse(gameState);
-    requestAnimationFrame(() => paintGame(gameState))
+  if (!gameActive) {
+    return;
+  }
+  gameState = JSON.parse(gameState);
+  requestAnimationFrame(() => paintGame(gameState));
 }
 
 function handleGameOver(data) {
-    if (!gameActive) {
-        return
-    }
-    data = JSON.parse(data);
-    if (data.winner === playerNumber) {
-        alert("You win!");
-    } else {
-        alert("You Lose!");
-    }
-    gameActive = false;
-};
+  if (!gameActive) {
+    return;
+  }
+  data = JSON.parse(data);
+
+  gameActive = false;
+
+  if (data.winner === playerNumber) {
+    alert('You Win!');
+  } else {
+    alert('You Lose :(');
+  }
+}
 
 function handleGameCode(gameCode) {
-    gameCodeDisplay.innerText = gameCode;
-};
+  gameCodeDisplay.innerText = gameCode;
+}
 
-function handleUnknownGame() {
-    reset();
-    alert('Unknown Game Code')
+function handleUnknownCode() {
+  reset();
+  alert('Unknown Game Code')
 }
 
 function handleTooManyPlayers() {
-    reset();
-    alert("This game is already in progress")
+  reset();
+  alert('This game is already in progress');
 }
 
 function reset() {
-    playerNumber = null;
-    gameCodeInput.value = "";
-    gameCodeDisplay.innerText = "";
-    initialScreen.style.display = "block";
-    gameScreen.style.display = "none";
+  playerNumber = null;
+  gameCodeInput.value = '';
+  initialScreen.style.display = "block";
+  gameScreen.style.display = "none";
 }
+
+//4.4.1 socket
+
+//4.5.3 boots
--- server/utils.js
+++ server/utils.js
@@ -1,13 +1,16 @@
 module.exports = {
-    makeid,
-  }
-  
-  function makeid(length) {
-     var result           = '';
-     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-     var charactersLength = characters.length;
-     for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-     }
-     return result;
-  }
+   makeid,
+ }
+ 
+ function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
+//  const socket = io('http://localhost:3000');
+//https://powerful-hollows-37907.herokuapp.com/
